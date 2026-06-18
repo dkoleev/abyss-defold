3834ff078a86d188ef2fb067b1f3a2d6002a0b43
dmitry.koleev <dmitry.koleev@gmail.com>
2026-06-18 12:22:11 +0300

    visual

---
+++ battle/damned/demon_eye/demon_eye.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/battle/damned/demon_eye/demon_eye.tilesource\"\n"
+  "}\n"
+  ""
+}
---
+++ objects/fire/fire.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/objects/fire/fire.tilesource\"\n"
+  "}\n"
+  ""
+}

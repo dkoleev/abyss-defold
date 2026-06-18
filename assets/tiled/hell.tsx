22beda61017c7b788778a895e667ab4f69309a64
dmitry.koleev <dmitry.koleev@gmail.com>
2026-06-18 07:34:45 +0300

    tiled

---
+++ assets/tiled/hell.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="hell" tilewidth="32" tileheight="32" tilecount="153" columns="17">
+ <image source="../backgrounds/hell_bg/hell_tiles.png" width="544" height="288"/>
+ <wangsets>
+  <wangset name="ground" type="corner" tile="-1">
+   <wangcolor name="ground_1" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>

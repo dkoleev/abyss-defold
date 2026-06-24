b0aaa433509f43ae0186d76d2a0c302d4bcbdb89
dmitry.koleev <dmitry.koleev@gmail.com>
2026-06-24 21:52:32 +0300

    test

---
+++ battle/rings/rings_opt1/ring_1_opt1.go
@@ -0,0 +1,15 @@
+components {
+  id: "ring"
+  component: "/battle/rings/ring.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"ring_005_32x32\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/rings_32x32.atlas\"\n"
+  "}\n"
+  ""
+}
--- battle/rings/rings_opt2/ring_1_opt2.go
+++ battle/rings/rings_opt2/ring_1_opt2.go
@@ -5,11 +5,15 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"ring_001_32x32\"\n"
+  data: "default_animation: \"ring_006_64x64\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/assets/rings_32x32.atlas\"\n"
+  "  texture: \"/assets/rings_64x64.atlas\"\n"
   "}\n"
   ""
+  scale {
+    x: 0.5
+    y: 0.5
+  }
 }

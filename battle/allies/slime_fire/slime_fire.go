f289c64dbb88d26c5a839eab154154dcaea03e52
dmitry.koleev <dmitry.koleev@gmail.com>
2026-07-03 16:55:32 +0300

    add slime fire

---
+++ battle/allies/slime_fire/slime_fire.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/battle/allies/slime_fire/slime_fire_idle.tilesource\"\n"
+  "}\n"
+  ""
+}
